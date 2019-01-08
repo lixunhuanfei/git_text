ced044a5edfcacc386010732d21fab4e00b35c28
zhangqain <1298784175@qq.com>
2019-01-08 18:03:56 +0800

    texi_test

---
+++ src/main/java/kmust/zsk/demo/GitTest.java
@@ -0,0 +1,15 @@
+package kmust.zsk.demo;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("何老三");
+		System.out.println("土狗");
+		System.out.println("山猪");
+		System.out.println("土贼");
+		
+
+	}
+
+}
